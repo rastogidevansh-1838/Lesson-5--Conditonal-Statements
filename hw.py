b1cc88fbe0b52c2fd4d21b3e8aa63af42cbbfad3
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-12-14 09:43:31 +0530

    hot and cold hw

---
+++ hw.py
@@ -0,0 +1,9 @@
+i = int(input("Enter the temparature : "))
+if (i > 25):
+ print("it is summer time don't wear winter clothes")
+else:
+ print("it is winter time pls don't dress up in summer clothes")   
+if (i > 65):
+ print("too hot i suggest you to shift to another cold country")
+if (i < -50):
+ print ("plsssss i beg you to shift to another hot country")
